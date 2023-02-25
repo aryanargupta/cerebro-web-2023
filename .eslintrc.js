16a2496b90689f1ebcc319c3e04fea2fdf2fd1d8
DhruvDave12 <202051064@iiitvadodara.ac.in>
2023-02-25 19:04:55 +0530

    :art: added eslint and prettier rules

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'object-curly-newline': 'off',
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="main__page">
+      <h1 className="main__page__test__h1">Welcome to Cerebro 2023</h1>
+    </div>
+  );
+}
+
+export default App;
