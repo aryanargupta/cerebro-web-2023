988057d7b97b989b94f248df4a37f805b09ab25e
Acoder22 <97425629+aryanargupta@users.noreply.github.com>
2023-03-15 04:52:55 +0000

    Login/Signup - Commit 1

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Formik, Form } from 'formik';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import AuthForm from "../../components/Authform/AuthForm";
+import FormInput from "../../components/FormInput/FormInput";
+import AuthBtn from "../../components/AuthButton/AuthButton";
 
 function Login() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+
+  const onSubmit = async (values, { setSubmitting, setFieldError }) => {
+    try {
+      const res = await axiosInstance.post("/account/login/", values);
+      auth.login(res.data.Token);
+      if (location.state?.from) navigate(location.state.from);
+      else navigate("/");
+    } catch (error) {
+      // console.log(error.response.data);
+      let message = "Invalid email or password";
+      if (!!error.response && !!error.response.data && !!error.response.data.status) {
+        message = error.response.data.status;
+        if (message === "User not verified") {
+          message = "Please verify your email first by clicking on the confirmation link sent to your email address during signup.";
+        }
+      }
+      setFieldError("authentication", message);
+    }
+    setSubmitting(false);
+  };
+
   return (
-    <>
-      <div>Login</div>
-      <div>pagee</div>
-    </>
+    <AuthForm title="Login" to="/signup" text="New member?" link="Sign Up">
+      <Formik {...{ validate, initialValues, onSubmit }}>
+        {({ isSubmitting, errors }) => (
+          <Form className="login">
+            {loginFormData.map(({ label, name, type }, index) => (
+              <FormInput {...{ name, label, type }} key={index} page="login" />
+            ))}
+            <Link to="/forgot-password" className="login__button">
+              <span className="login__button__link">Forgot Password?</span>
+            </Link>
+            <AuthBtn {...{ errors, isSubmitting }} btnText="Login" />
+          </Form>
+        )}
+      </Formik>
+    </AuthForm>
   );
 }
 
