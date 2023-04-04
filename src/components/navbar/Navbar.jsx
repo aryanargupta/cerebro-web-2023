6d854799608f68775bac041ed0f9b24f2be8bc69
Acoder22 <97425629+aryanargupta@users.noreply.github.com>
2023-04-04 06:45:57 +0000

    Made hamburger functional, fixed design and dashboard

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Navbar.scss';
 
 function Navbar({ isLandingPage }) {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
   function navigateToDashboard() {
     navigate('/dashboard');
   }
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <>
-      <div className={`hamburger${isLandingPage ? '_landing' : ''}`}> </div>
+      <div className={`hamburger${isLandingPage ? '_landing' : ''}`} onClick={toggleMenu}>
+        <div className="hamburger-line"></div>
+        <div className="hamburger-line"></div>
+        <div className="hamburger-line"></div>
+      </div>
       <button className="dashboard-btn" onClick={navigateToDashboard} type="button">
         Dashboard
       </button>
+      {showMenu && (
+        <div className="menu">
+          <a href="/faqs">FAQs</a>
+          <a href="/timeline">Timeline</a>
+          <a href="/events">Events</a>
+          <a href="/">Home</a>
+          <a href="/team">Teams</a>
+        </div>
+      )}
     </>
   );
 }
